3d4cae3907980e349d9eb7ae0860df8c8e468ff5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-20 15:50:33 +0100

    Infinix-X6823C: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: infinix Model: Infinix X6823C Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X6823C.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X6823C-user \
+    omni_Infinix-X6823C-userdebug \
+    omni_Infinix-X6823C-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Infinix Infinix X6823C (Infinix-X6823C)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
